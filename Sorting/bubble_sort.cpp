8005f5acff62e17978c65be95631ba01cd209cd8
Aadeesh Bali <officialaadeesh76@gmail.com>
2026-05-28 09:43:05 +0530

    started after long time

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -5,6 +5,7 @@
 // Better for nearly sorted or very small input array as it gets it done in O(n)
 
 void bubbleSort(int *arr, int size);
+void swap(int *arr, int ind1, int ind2);
 
 int main()
 {
@@ -20,20 +21,23 @@ int main()
 
 void bubbleSort(int *arr, int size)
 {
-    for (int i = 0; i < size - 1; i++)
+    for (int i = 0; i < size; i++)
     {
-        bool is_swapped = false;
         for (int j = 1; j < size - i; j++)
         {
             if (arr[j] < arr[j - 1])
             {
-                is_swapped = true;
-                std::swap(arr[j], arr[j - 1]);
+                swap(arr, j, j - 1);
             }
         }
-        if (!is_swapped)
-        {
-            break;
-        }
     }
+    return;
+}
+
+void swap(int *arr, int ind1, int ind2)
+{
+    int temp = arr[ind1];
+    arr[ind1] = arr[ind2];
+    arr[ind2] = temp;
+    return;
 }
